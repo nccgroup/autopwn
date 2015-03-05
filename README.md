530f83179e54521a5034330944875fc9136acc1e
root <root@localhost.localdomain>
2015-03-05 09:05:28 +0000

    Introduced assessments configuration

--- README.md
+++ README.md
@@ -1,24 +1,2 @@
 # autopwn
 Specify targets and run sets of tools against them
-
-By Aidan Marlin (email: aidan [dot] marlin [at] nccgroup [dot] com).
-
-autopwn expects one argument, the name of the file containing
-targets to be pwned. Format of file should be
-<ip>#<domain name>#[port-for-dirb]#[ssl-for-dirb] where
-[ssl-for-dirb] would be 'http' or 'https'.
-
-If target does not have a domain name, specify IP in both fields..
-This might be fixed later..
-
-Examples:
-195.95.131.71#nccgroup.com#443#https
-216.58.208.78#google.com#80#http
-
-autopwn uses the tools/ directory located where this script is
-to load tool definitions, which are yaml files. You can find
-some examples in the directory already. If you think one is
-missing, mention it on GitHub or email me and I might add it.
-
-Have fun!
-Legal purposes only..
--- autopwn.py
+++ autopwn.py
@@ -25,6 +25,23 @@ from time import gmtime, strftime
 # 4b) Have online repo where users can share yaml
 #     configs
 # 5 - What if tool requires root? Make check for this
+# Add support for:
+# - enum4linux
+# - nbtscan
+# - sslyze
+# - smb_scan
+# - w3af ?
+# - snmpwalk/check/etc
+# - dotdotpwn
+# - wp-scan
+# - davtest?
+# - DNS tools
+# - Harvester?
+# 6 - Command line arguments?
+# 7 - Option not to run tools in parallel?
+# 8 - Assessment type selection then just run tools?
+#     Why should user be able to select tools? If a new combo
+#     is needed why not just create it?
 
 class executeToolThread (threading.Thread):
    def __init__(self, thread_ID, tool_name, tool_binary_location, tool_arguments):
@@ -39,7 +56,7 @@ class executeToolThread (threading.Thread):
       executeTool(self.thread_ID, self.tool_name, self.tool_binary_location, self.tool_arguments)
       print "[-] " + self.tool_name + " is done.."
 
-#executeToolThread(1, tool[0], tool[1], tool[3])
+#executeToolThread(1, tool[0], tool[1], tool[2])
 def executeTool(thread_ID, tool_name, tool_binary_location, tool_arguments):
    os.system(tool_binary_location + " " + tool_arguments)
 
@@ -95,15 +112,15 @@ def getTargets(target):
 
    return target
 
-def determineAssessment(config, menu_items):
-   print "What kind of assessment is this?"
+def determineAssessment(config_tools, menu_items):
+   print "What assessment do you want to run?"
    for index, item in enumerate(menu_items):
       if item != 0:
          print str(index) + ") " + str(item)
 
    as_type = raw_input('Choose > ')
    if as_type == '':
-      print config
+      print config_tools
       print menu_items
       sys.exit(1)
    else:
@@ -111,39 +128,19 @@ def determineAssessment(config, menu_items):
 
    return as_type_chosen
 
-def chooseTools(config, as_type, menu_items):
+def determineTools(config_tools, config_assessments, as_type, menu_items):
    tool_subset = []
-   tool_subset_chosen = []
 
-   # We need to isolate the tools in the selected assessment type
-   # otherwise we won't get consistent tool number increments
-   for index, tool in enumerate(config):
-      if tool[2] == menu_items[as_type]:
+   for tool in config_tools:
+      if tool[0] in config_assessments[as_type][1]:
          tool_subset.append(tool)
 
-   print "Which tools would you like to run for " + menu_items[as_type] + "?"
-   for index, tool in enumerate(tool_subset):
-      print str(index) + ") " + tool[0]
-
-   print "You can choose more than one like (0,1,2,3)"
-   tools_chosen_input = raw_input('Choose > ')
-
-   # Split
-   tools_chosen_input = re.split(',',tools_chosen_input)
-   # Convert int
-   tools_chosen_input = [int(a) for a in tools_chosen_input] 
-
-   # Determine subset of tools which were chosen
-   for index, tool in enumerate(tool_subset):
-      if index in tools_chosen_input:
-         tool_subset_chosen.append(tool)
-
-   return tool_subset_chosen
+   return tool_subset
 
 def checkToolExists(tool):
    #                                      Don't judge me
-   if 0 != os.system("which " + tool[1] + " 2>/dev/null"):
-      if 0 != os.system("ls " + tool[1] + " 2>/dev/null"):
+   if 0 != os.system("which " + tool[1] + " >/dev/null 2>&1"):
+      if 0 != os.system("ls " + tool[1] + " >/dev/null 2>&1"):
          print tool[1] + " was not found. Quitting.."
          sys.exit(1)
 
@@ -165,10 +162,10 @@ def runTools(tools, target):
          target_protocol = host[3]
 
          # Let's do it
-         tool[3] = tool[3].format(target_domain_name=target_domain_name, target_ip=target_ip, date=date, port_number=port_number, target_protocol=target_protocol)
-
+         tool[2] = tool[2].format(target_domain_name=target_domain_name, target_ip=target_ip, date=date, port_number=port_number, target_protocol=target_protocol)
+         time.sleep (50.0 / 1000.0);
          #                           Thread ID  Name     Bin loc  args
-         thread.append(executeToolThread(index, tool[0], tool[1], tool[3]))
+         thread.append(executeToolThread(index, tool[0], tool[1], tool[2]))
          # Start threads
          thread[index].start()
          index = index + 1
@@ -178,32 +175,52 @@ def getCurrentUser():
       print "nmap requires uid 0 (root)"
       sys.exit(1)
 
-def getConfig(config, menu_items):
+def getConfig(config_tools,config_assessments,menu_items):
    i = 0
    pathname = ""
    tools_dir = ""
+   assessment_dir = ""
 
    pathname = os.path.dirname(sys.argv[0])
    tools_dir = os.path.abspath(pathname) + "/tools/"
+   assessments_dir = os.path.abspath(pathname) + "/assessments/"
 
+   # Pull tool configs
    for file in os.listdir(tools_dir):
       if file.endswith(".apc"):
          stream = open(tools_dir + file, 'r')
          objects = yaml.load(stream)
 
          k = 0
-         config[i][k] = objects['name']
+         config_tools[i][k] = objects['name']
          k = k + 1
-         config[i][k] = objects['binary_location']
+         config_tools[i][k] = objects['binary_location']
          k = k + 1
-         config[i][k] = objects['menu']
-         k = k + 1
-         config[i][k] = objects['arguments']
+         config_tools[i][k] = objects['arguments']
 
       i = i + 1
 
-   for tool_menu_owner in config:
-      menu_items.append(tool_menu_owner[2])
+   #Reset
+   i = 0
+   # Pull assessment configs
+   for file in os.listdir(assessments_dir):
+      if file.endswith(".apc"):
+         stream = open(assessments_dir + file, 'r')
+         objects = yaml.load(stream)
+
+         k = 0
+         config_assessments[i][k] = objects['name']
+         k = k + 1
+         config_assessments[i][k] = objects['tools']
+         k = k + 1
+         config_assessments[i][k] = objects['menu_name']
+         k = k + 1
+         config_assessments[i][k] = objects['thread']
+
+      i = i + 1
+
+   for config_assessment_menu_item in config_assessments:
+      menu_items.append(config_assessment_menu_item[2])
 
 def fixMenuItems(menu_items):
    # TODO Find and remove 0
@@ -212,26 +229,27 @@ def fixMenuItems(menu_items):
 
 def showTools(tools,target):
    for tool in tools:
-      print tool[1] + " " + tool[3]
+      print tool[1] + " " + tool[2]
 
 def main():
    # Variable declarations
    target = []
    tools = []
-   config = [[0 for x in range(16)] for x in range(256)] 
+   config_tools = [[0 for x in range(16)] for x in range(256)] 
+   config_assessments = [[0 for x in range(16)] for x in range(256)] 
    as_type = 0
    menu_items = []
 
    # Function calls
    getTargets(target)
 
-   # Get configs
-   getConfig(config,menu_items)
+   # Get config_toolss
+   getConfig(config_tools,config_assessments,menu_items)
    # Remove dupes and sort menu
    menu_items = fixMenuItems(menu_items)
 
-   as_type = determineAssessment(config,menu_items)
-   tools = chooseTools(config,as_type,menu_items)
+   as_type = determineAssessment(config_tools,menu_items)
+   tools = determineTools(config_tools,config_assessments,as_type,menu_items)
 
    # Tools to be run
    showTools(tools,target)
