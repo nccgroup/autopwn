c88271d09bb0e1fda368d80e4df3695a011a1d6c
Aidan Marlin <aidan.marlin@gmail.com>
2015-11-22 12:02:35 +0000

    Revised assessment approach

--- autopwn/__init__.py
+++ autopwn/__init__.py
@@ -18,7 +18,6 @@ from subprocess import Popen, PIPE
 
 # TODO
 #      - TLS
-#      - Assessments
 app = Flask(__name__, static_url_path=os.path.dirname(os.path.abspath(__file__)))
 api = Api(app)
 
@@ -106,7 +105,34 @@ class Assessments(Resource):
 
         # Add tools to assessment
         for i, assessment in enumerate(data['result']):
-            cur.execute("SELECT tool FROM assessment_tools WHERE assessment = ?",(str(i),))
+            cur.execute("SELECT tool FROM assessment_tools WHERE assessment = ?",(str(i+1),))
+            tool_ids = dict(result=[dict(r) for r in cur.fetchall()])
+            assessment['tools'] = tool_ids['result']
+
+        # Close connection
+        if con:
+            con.close()
+        return data
+
+    # Submit a new tool
+    def post(self):
+        args = parser.parse_args()
+        return args
+
+class AssessmentsId(Resource):
+    # Get assessments
+    def get(self, assessment_id):
+        args = parser.parse_args()
+        con = sqlite3.connect(os.path.dirname(os.path.abspath(__file__)) + '/assets.db')
+        con.row_factory = sqlite3.Row
+        cur = con.cursor()
+
+        cur.execute("SELECT * FROM assessments WHERE id = ?",(assessment_id),)
+        data = dict(result=[dict(r) for r in cur.fetchall()])
+
+        # Add tools to assessment
+        for i, assessment in enumerate(data['result']):
+            cur.execute("SELECT tool FROM assessment_tools WHERE assessment = ?",(str(i+1),))
             tool_ids = dict(result=[dict(r) for r in cur.fetchall()])
             assessment['tools'] = tool_ids['result']
 
@@ -133,6 +159,27 @@ class Tools(Resource):
             cur.execute("SELECT * FROM tools WHERE name LIKE ? OR description LIKE ?",('%' + args['search'] + '%','%' + args['search'] + '%'))
         else:
             cur.execute("SELECT * FROM tools")
+        data = dict(result=[dict(r) for r in cur.fetchall()])
+
+        # Close connection
+        if con:
+            con.close()
+        return data
+
+    # Submit a new tool
+    def post(self):
+        args = parser.parse_args()
+        return args
+
+class ToolsId(Resource):
+    # Get tools
+    def get(self, tool_id):
+        args = parser.parse_args()
+        con = sqlite3.connect(os.path.dirname(os.path.abspath(__file__)) + '/assets.db')
+        con.row_factory = sqlite3.Row
+        cur = con.cursor()
+
+        cur.execute("SELECT * FROM tools WHERE id =?",(tool_id,))
         data = dict(result=[dict(r) for r in cur.fetchall()])
 
         # Close connection
@@ -215,7 +262,6 @@ class ToolsJobsIdExecute(Resource):
         job_data = dict(result=[dict(r) for r in cur.fetchall()])
 
         # Index is now tied to database schema, yuck
-        print(args['id'])
         job = job_data['result'][0]
         # TODO Allow set in config file
         job['tools_directory'] = "/root/tools"
@@ -227,8 +273,9 @@ class ToolsJobsIdExecute(Resource):
         try:
             os.makedirs(job['output_dir'])
         except OSError as e:
-            if e.errno == errno.EEXIST:
-                return {'message':'Directory exists'}, 500
+            pass
+            #if e.errno == errno.EEXIST:
+            #    return {'message':'Directory exists'}, 500
 
         tool['id'] = job['tool']
         # Get dependencies
@@ -260,129 +307,6 @@ class ToolsJobsIdExecute(Resource):
         thread[-1].start()
 
         return {'message':'Tool executed'}, 201
-
-class AssessmentsJobs(Resource):
-    # List jobs
-    def get (self):
-        args = parser.parse_args()
-        con = sqlite3.connect(os.path.dirname(os.path.abspath(__file__)) + '/assets.db')
-        con.row_factory = sqlite3.Row
-        cur = con.cursor()
-
-        # If /assessments/jobs?search=xxx not specified then SELECT *
-        if args['search'] != None:
-           cur.execute("SELECT * FROM assessment_jobs WHERE tool LIKE ? OR target LIKE ? OR target_name LIKE ? OR protocol LIKE ? OR port_number LIKE ? OR user like ? OR password LIKE ? OR user_file LIKE ? OR password_file LIKE ?",('%' + args['search'] + '%','%' + args['search'] + '%','%' + args['search'] + '%','%' + args['search'] + '%','%' + args['search'] + '%','%' + args['search'] + '%','%' + args['search'] + '%','%' + args['search'] + '%','%' + args['search'] + '%'))
-        else:
-           cur.execute("SELECT * FROM assessment_jobs")
-        data = dict(result=[dict(r) for r in cur.fetchall()])
-
-        # Close connection
-        if con:
-            con.close()
-
-        return data
-
-    # Submit new job
-    def post(self):
-        args = parser.parse_args()
-        con = sqlite3.connect(os.path.dirname(os.path.abspath(__file__)) + '/assets.db')
-        cur = con.cursor()
-
-        # curl -i --data "assessment=1&target=localhost&target_name=target_name&protocol=https&port_number=1337&user=a_user&password=a_password&user_file=/user/file&password_file=/password/file" http://127.0.0.1:5000/assessments/jobs
-        cur.execute("INSERT INTO assessment_jobs(assessment,target,target_name,protocol,port_number,user,password,user_file,password_file) VALUES(?,?,?,?,?,?,?,?,?)",(args['assessment'],args['target'],args['target_name'],args['protocol'],args['port_number'],args['user'],args['password'],args['user_file'],args['password_file']))
-        data = cur.lastrowid
-        con.commit()
-
-        # Close connection
-        if con:
-            con.close()
-
-        return { 'id':data }, 201
-
-# List individual jobs
-class AssessmentsJobsId(Resource):
-    def get(self, job_id):
-        con = sqlite3.connect(os.path.dirname(os.path.abspath(__file__)) + '/assets.db')
-        con.row_factory = sqlite3.Row
-        cur = con.cursor()
-
-        cur.execute("SELECT * FROM assessment_jobs WHERE id = ?",(job_id,))
-        data = dict(result=[dict(r) for r in cur.fetchall()])
-
-        # Close connection
-        if con:
-            con.close()
-
-        return data
-
-# Execute job
-class AssessmentsJobsIdExecute(Resource):
-    def post(self):
-        # Process placeholders
-        assessment = {}
-        job = {}
-        args = parser.parse_args()
-        con = sqlite3.connect(os.path.dirname(os.path.abspath(__file__)) + '/assets.db')
-        con.row_factory = sqlite3.Row
-        cur = con.cursor()
-
-        # Get job id columns
-        cur.execute("SELECT * FROM assessment_jobs WHERE id = ?",(args['id'],))
-        job_data = dict(result=[dict(r) for r in cur.fetchall()])
-
-        # Index is now tied to database schema, yuck
-        job = job_data['result'][0]
-        # TODO Allow set in config file
-        job['tools_directory'] = "/root/tools"
-        job['date'] = strftime("%Y%m%d_%H%M%S%z")
-        job['output_dir'] = os.path.dirname(os.path.abspath(__file__)) + \
-                                '/' + strftime("%Y%m%d") + \
-                                "_autopwn_" + \
-                                job_data['result'][0]['target_name']
-        # TODO Uncomment
-        try:
-            os.makedirs(job['output_dir'])
-        except OSError as e:
-            if e.errno == errno.EEXIST:
-                return {'message':'Directory exists'}, 500
-
-        assessment['id'] = job['assessment']
-        print("DEBUG: assessment['id'] == " + str(assessment['id']))
-        cur.execute("SELECT tool FROM assessment_tools WHERE assessment = ?",(str(assessment['id']),))
-        tool_ids = dict(result=[dict(r) for r in cur.fetchall()])
-        assessment['tools'] = tool_ids['result']
-
-        for i, data in enumerate(assessment['tools']):
-            print("cunt")
-            # Get dependencies
-            cur.execute("SELECT dependency from dependencies WHERE tool = ?",(data['tool'],))
-            dependency = dict(result=[dict(r) for r in cur.fetchall()])
-
-            # Get tool execute string
-            cur.execute("SELECT * FROM tools WHERE id = ?",(data['tool'],))
-            tool_data = dict(result=[dict(r) for r in cur.fetchall()])
-            tool = tool_data['result'][0]
-
-            ddict_options = defaultdict(lambda:'')
-            for option in job:
-                ddict_options[option] = job[option]
-
-            tool['execute_string'] = tool['execute_string'].format(**ddict_options)
-            print(tool['execute_string'])
-
-            thread = []
-            # Tool string generated, execute
-            thread.append(RunThreads(tool,job,'assessment'))
-            # If main process dies, everything else *SHOULD* as well
-            thread[-1].daemon = True
-            # Start threads
-            thread[-1].start()
-
-        # Close connection
-        if con:
-            con.close()
-
-        return {'message':'Assessment executed'}, 201
 
 class Dependencies(Resource):
     def get(self):
@@ -429,40 +353,6 @@ class OptionsId(Resource):
             con.close()
 
         return data
-
-class AssessmentsExports(Resource):
-    def get(self):
-        con = sqlite3.connect(os.path.dirname(os.path.abspath(__file__)) + '/assets.db')
-        con.row_factory = sqlite3.Row
-        cur = con.cursor()
-
-        cur.execute("SELECT id, zip_file FROM assessment_jobs")
-        data = dict(result=[dict(r) for r in cur.fetchall()])
-        print(data)
-
-        # Close connection
-        if con:
-            con.close()
-
-        return data
-
-# Retrieve output for job id
-class AssessmentsExportsId(Resource):
-    def get(self, job_id):
-        con = sqlite3.connect(os.path.dirname(os.path.abspath(__file__)) + '/assets.db')
-        con.row_factory = sqlite3.Row
-        cur = con.cursor()
-
-        cur.execute("SELECT zip_file FROM assessment_jobs WHERE id = ?",(job_id,))
-        data = dict(result=[dict(r) for r in cur.fetchall()])
-        zip_file = data['result'][0]['zip_file'] + '.zip'
-
-        # Close connection
-        if con:
-            con.close()
-
-        return send_file(zip_file, as_attachment=True)
-
 
 class ToolsExports(Resource):
     def get(self):
@@ -519,19 +409,16 @@ api.add_resource(Pong, '/ping')
 # Fetch all tools
 # curl -i http://127.0.0.1:5000/tools
 api.add_resource(Tools, '/tools')
+# Fetch tool id
+# curl -i http://127.0.0.1:5000/tools/1
+api.add_resource(ToolsId, '/tools/<tool_id>')
 # Fetch all assessments
 # curl -i http://127.0.0.1:5000/assessments
 api.add_resource(Assessments, '/assessments')
-
-# Fetch all assessment jobs
-# curl -i http://127.0.0.1:5000/assessments/jobs
-api.add_resource(AssessmentsJobs, '/assessments/jobs')
 # Fetch assessment id
-# curl -i http://127.0.0.1:5000/assessments/jobs/1
-api.add_resource(AssessmentsJobsId, '/assessments/jobs/<job_id>')
-# Execute assesment job id
-# curl -i --data "id=1" http://127.0.0.1:5000/assessments/jobs/execute
-api.add_resource(AssessmentsJobsIdExecute, '/assessments/jobs/execute')
+# curl -i http://127.0.0.1:5000/assessments/1
+api.add_resource(AssessmentsId, '/assessments/<assessment_id>')
+
 
 # Fetch all tool jobs
 # curl -i http://127.0.0.1:5000/jobs
@@ -556,13 +443,6 @@ api.add_resource(Options, '/options')
 # curl -i http://127.0.0.1:5000/options/1
 api.add_resource(OptionsId, '/options/<tool_id>')
 
-# Fetch all assessment outputs
-# curl -i http://127.0.0.1:5000/exports
-api.add_resource(AssessmentsExports, '/assessments/jobs/exports')
-# Fetch assessment output from job id
-# curl -i http://127.0.0.1:5000/exports/1
-api.add_resource(AssessmentsExportsId, '/assessments/jobs/exports/<job_id>')
-
 # Fetch all tool outputs
 # curl -i http://127.0.0.1:5000/exports
 api.add_resource(ToolsExports, '/tools/jobs/exports')
@@ -581,7 +461,7 @@ def main():
         app.run(host='0.0.0.0', debug=True,threaded=True)
     else:
         #app.run(debug=True,threaded=True,ssl_context=context)
-        app.run(debug=True,threaded=True,port=5001)
+        app.run(debug=True,threaded=True,port=5000)
 
 if __name__ == '__main__':
     main()
